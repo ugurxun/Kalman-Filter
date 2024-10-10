a96c479b9deaa1a7789ff93b1ed5694549e014b7
Uğur ÜN <50785602+ugurxun@users.noreply.github.com>
2024-10-10 16:45:40 +0300

    Comment line specifier(#) updated as %

--- LKF/1D-LKF/temperature_liquid_tank.m
+++ LKF/1D-LKF/temperature_liquid_tank.m
@@ -3,8 +3,8 @@ clc;
 
 true_temp = [50.005, 49.994, 49.993, 50.001, 50.006, 49.998, 50.021, 50.005, 50,  49.997];
 meas_temp =  [49.986, 49.963, 50.09, 50.001, 50.018, 50.05, 49.938, 49.858, 49.965, 50.114];
-#true_temp =  [50.505,50.994, 51.493, 52.001, 52.506, 52.998, 53.521, 54.005, 54.5,54.997]
-#meas_temp =  [50.486, 50.963, 51.597, 52.001, 52.518,53.05, 53.438, 53.858, 54.465,55.114]
+%true_temp =  [50.505,50.994, 51.493, 52.001, 52.506, 52.998, 53.521, 54.005, 54.5,54.997]
+%meas_temp =  [50.486, 50.963, 51.597, 52.001, 52.518,53.05, 53.438, 53.858, 54.465,55.114]
 
 
 
@@ -17,30 +17,30 @@ q = 0.0001; # process noise variance
 r = 0.01  ; # measurement noise variance
 
 
-#Initilization
+%Initilization
 x_i = 60;
 std_init = 100;
 p_i = std_init ^ 2;
 
-#initial prediction
+%initial prediction
 x_i = x_i;
 p_i = p_i + q;
 
 for i = 1:n_of_meas
 
-    #update
+    %update
     k(i) = p_i/(p_i + r);
     est(i) = x_i + k(i)*(meas_temp(i) - x_i);
     p_i = (1 - k(i))*p_i;
 
-    #predict
+    %predict
     x_i = est(i);
     p_i = p_i + q;
 end
 
 
-#PLOTS
-#Estimation plots
+%PLOTS
+%Estimation plots
 figure(1)
 plot(t, est, 'r-','Linewidth',2);hold on
 plot(t, meas_temp, 'b-','Linewidth',2);hold on
@@ -52,7 +52,7 @@ xlim([t(1) t(end)])
 grid on
 
 
-#Kalman Gain plot
+%Kalman Gain plot
 figure(2)
 plot(t, k, 'b-','Linewidth',2)
 title('Temperature Estimates Kalman Gain of Liquid Tank')
